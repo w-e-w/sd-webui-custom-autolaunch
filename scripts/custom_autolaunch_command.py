c90bbe03f475bc20c28e07443c916dd8126d0ad1
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-09-15 07:56:11 +0900

    add validation command integrity

--- scripts/custom_autolaunch_command.py
+++ scripts/custom_autolaunch_command.py
@@ -2,8 +2,12 @@ from modules import script_callbacks, shared
 from pathlib import Path
 import gradio as gr
 import subprocess
+import hashlib
 import shlex
 import os
+
+extension_root = Path(__file__).resolve().parents[1]
+check_command_path = extension_root.joinpath('.check_command')
 
 
 class OptionMarkdown(shared.OptionInfo):
@@ -12,14 +16,28 @@ class OptionMarkdown(shared.OptionInfo):
         self.do_not_save = True
 
 
-class ReadOnlyMode(Exception):
+class CustomAutoLaunch(Exception):
     def __init__(self, msg):
         super().__init__(msg)
 
 
-def permission_denied():
-    if not shared.opts.disable_extension_access:
-        raise ReadOnlyMode('[Permission Denied]: Custom autolaunch command is in Read-only mode')
+def confirm_permission():
+    if shared.cmd_opts.disable_extension_access:
+        raise CustomAutoLaunch('[Permission Denied]: Custom autolaunch command is in Read-only mode')
+    else:
+        command_hash = hashlib.sha256(shared.opts.custom_autolaunch_command.encode("utf8")).hexdigest()
+        with open(check_command_path, 'w', encoding='utf-8') as file:
+            file.write(f'{command_hash}{shared.opts.custom_autolaunch_command}')
+
+
+def validate_command_integrity():
+    try:
+        with open(check_command_path, 'r', encoding='utf-8') as file:
+            read_check = file.read()
+        if read_check[:64] == hashlib.sha256(read_check[64:].encode("utf8")).hexdigest() and read_check[64:] == shared.opts.custom_autolaunch_command:
+            return True
+    except Exception as e:
+        raise CustomAutoLaunch(f'{e}\n[Warning]: custom autolaunch command validation failed due to above reason\nskip executing custom command.')
 
 
 OptionInfo_command = (shared.OptionInfo(None,
@@ -27,7 +45,7 @@ OptionInfo_command = (shared.OptionInfo(None,
                                         component=gr.Textbox,
                                         restrict_api=True,
                                         component_args={'interactive': not shared.cmd_opts.disable_extension_access},
-                                        onchange=permission_denied if shared.cmd_opts.disable_extension_access else None,
+                                        onchange=confirm_permission,
                                         ))
 
 
@@ -36,7 +54,7 @@ if shared.cmd_opts.disable_extension_access:
     OptionInfo_command.info('Read-only')
 
 
-with open(Path(__file__).resolve().parents[1].joinpath('explanation.md'), 'r') as f:
+with open(extension_root.joinpath('explanation.md'), 'r') as f:
     option_explanation_md = OptionMarkdown(f.read())
 
 shared.options_templates.update(shared.options_section(('system', 'System'), {
@@ -52,12 +70,11 @@ def run_custom_launch_command(demo, _app, *_args, **_kwargs):
             command = shlex.split(shared.opts.custom_autolaunch_command.format(url=demo.local_url))
             subprocess.Popen(command)
     except Exception as e:
-        print(
-            f'{e}\n[ERROR]: Failed custom autolaunch: with command {shared.opts.custom_autolaunch_command}')
+        print(f'{e}\n[ERROR]: Failed custom autolaunch: with command {shared.opts.custom_autolaunch_command}')
 
 
 def check_autolaunch(*_args, **_kwargs):
-    if shared.opts.custom_autolaunch_enable:
+    if shared.opts.custom_autolaunch_enable and shared.opts.custom_autolaunch_command:
         auto_launch = False
         env_sd_webui_restarting = os.getenv('SD_WEBUI_RESTARTING') or os.getenv('SD_WEBUI_RESTARTING ')
         if env_sd_webui_restarting != '1':
@@ -73,7 +90,7 @@ def check_autolaunch(*_args, **_kwargs):
         elif shared.cmd_opts.autolaunch:  # webui < 1.6.0
             auto_launch = True
 
-        if auto_launch:
+        if auto_launch and validate_command_integrity():
             os.environ.setdefault('SD_WEBUI_RESTARTING', '1')
             os.environ.setdefault('SD_WEBUI_RESTARTING ', '1')
             script_callbacks.on_app_started(run_custom_launch_command)
